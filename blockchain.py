00216b0726514aad64c9114dbc5cc07b0ea5f2ce
McCrearyD <mccreary@dyllan.ai>
2021-02-10 00:37:31 -0800

    added arg types & register node

--- blockchain.py
+++ blockchain.py
@@ -1,17 +1,19 @@
 from time import time
 import json
 import hashlib
+from urllib.parse import urlparse
 
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # genesis block
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self, proof, previous_hash=None):
+    def new_block(self, proof: int, previous_hash: str = None) -> dict:
         """create a new block in the Blockchain
 
         proof (int): proof given by the "proof of work" algorithm
@@ -51,11 +53,11 @@ class Blockchain:
         return self.last_block['index'] + 1
 
     @property
-    def last_block(self):
+    def last_block(self) -> dict:
         return self.chain[-1]
 
     @staticmethod
-    def hash(block):
+    def hash(block: dict) -> str:
         """creates a SHA-256 hash of a Block
 
         block (dict): block
@@ -67,7 +69,7 @@ class Blockchain:
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    def proof_of_work(self, last_proof):
+    def proof_of_work(self, last_proof: int) -> int:
         """proof of work algorithm:
         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
         - p is the previous proof, and p' is the new proof
@@ -83,7 +85,7 @@ class Blockchain:
         return proof
 
     @staticmethod
-    def valid_proof(last_proof, proof):
+    def valid_proof(last_proof: int, proof: int) -> bool:
         """Validates the Proof: Does hash(last_proof, proof) contain 6969 at the end?
 
         last_proof (int): previous proof
@@ -95,3 +97,12 @@ class Blockchain:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "6969"
+
+    def register_node(self, address: str):
+        """add a new node to the list of nodes
+
+        address (str): address of node ie. 'http://192.168.0.5:5000'
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
